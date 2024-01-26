102876195772e644133e515c8aaead606a91a7bf
Arthur Camu <arthur.camu@gmail.com>
2024-01-26 14:46:38 +0100

    Trying to solve data leak issue with st.cache_data on main function

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import os
 import zipfile
 from datetime import datetime as dt
 import io
-import pytz
 
 @st.cache_data
 def load_image(img):
@@ -106,6 +105,7 @@ def image_to_vignette(img, overlay):
 
     return image_to_circle(square_crop_in_center(Image.alpha_composite(overlay, img_resized_tbg),inner_px_zoom=1))
 
+@st.cache_data
 def main(cercle,vignette):
     # Set valid format for upload, then upload
     valid_images = [".jpg",".jpeg", ".png"]
